920c73dabbc98719cf941cf56d34387fb704249d
Neil Ryan <neilryanlipaod@gmail.com>
2021-09-21 19:25:37 +0800

    ADDED factory to parse json

--- lib/widgets/models/haiku.dart
+++ lib/widgets/models/haiku.dart
@@ -4,4 +4,8 @@ class Haiku {
   String haiku;
 
   Haiku({required this.id, required this.author, required this.haiku});
+
+  factory Haiku.fromJson(Map<String, dynamic> json) {
+    return Haiku(id: json['id'], author: json['author'], haiku: json['haiku']);
+  }
 }
